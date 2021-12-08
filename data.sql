148993c2fe8916c8fa78477129ef4186129ee944
Tchilo <tchiloross@gmail.com>
2021-12-08 23:18:34 +0200

    Add transaction file and add data

--- data.sql
+++ data.sql
@@ -8,3 +8,15 @@ INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
 VALUES ('Pikachu', '01/07/2021', 15.04,false, 1 );
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
 VALUES ('Devimon', '05/12/2017', 11, true, 5);
+INSERT INTO mascota (name, date_of_birth, weight_kg, neutered, escape_attempts)
+values('Charmander', '02/08/2020', 11, false, 0);
+INSERT INTO mascota (name, date_of_birth, weight_kg, neutered, escape_attempts)
+values('Plantmon', '15/11/2022', 5.7, true, 2);
+INSERT INTO mascota (name, date_of_birth, weight_kg, neutered, escape_attempts)
+values('Squirtle', '02/04/1993', 12.13, false, 3);
+INSERT INTO mascota (name, date_of_birth, weight_kg, neutered, escape_attempts)
+values('Angemon', '12/01/2005', 45, true, 1);
+INSERT INTO mascota (name, date_of_birth, weight_kg, neutered, escape_attempts)
+values('Boarmon', '07/06/2005', 20.4, true, 7);
+INSERT INTO mascota (name, date_of_birth, weight_kg, neutered, escape_attempts)
+values('Blossom', '13/10/1998', 17, true, 3);
