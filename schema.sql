997748dfae52c23953b5da41698ce4700b7a26b6
Tchilo <tchiloross@gmail.com>
2021-12-08 11:54:05 +0200

    Add queries

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id integer PRIMARY KEY,
+    id BIGSERIAL NOT NULL PRIMARY KEY,
     name varchar(100),
     date_of_birth DATE,
     escape_attempts integer,
