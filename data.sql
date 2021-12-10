fe47d375fbc76ef0a700bc077bc213d57bf40846
Tchilo <tchiloross@gmail.com>
2021-12-10 23:22:31 +0200

    Add specializations, vets and visits table

--- data.sql
+++ data.sql
@@ -2,42 +2,68 @@
 
 -- owners info
 INSERT INTO owners (full_name, age )
-VALUES ('Sam Smith', 34);
-INSERT INTO owners (full_name, age )
-VALUES ('Jennifer Orwell', 19);
-INSERT INTO owners (full_name, age )
-VALUES ('Bob', 45);
-INSERT INTO owners (full_name, age )
-VALUES ('Melody Pond', 77);
-INSERT INTO owners (full_name, age )
-VALUES ('Dean Winchester', 14);
-INSERT INTO owners (full_name, age )
+VALUES ('Sam Smith', 34),
+VALUES ('Jennifer Orwell', 19),
+VALUES ('Bob', 45),
+VALUES ('Melody Pond', 77),
+VALUES ('Dean Winchester', 14),
 VALUES ('Jodie Whittaker', 38);
 
 -- species info
 INSERT INTO species (name)
-values('Pokemon');
-INSERT INTO species (name)
+values('Pokemon'),
 VALUES('Digimon');
 
 -- animal data
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species_id, owner_id)
-VALUES ('Agumon', '02/03/2020', 10.23, true, 0, 2, 1);
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species_id, owner_id)
-VALUES ('Gabumon', '11/15/2018', 5, true, 2, 2, 2);
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species_id, owner_id)
-VALUES ('Pikachu', '01/07/2021', 15.04,false, 1, 1,2 );
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species_id, owner_id)
-VALUES ('Devimon', '05/12/2017', 11, true, 5, 2,3);
-INSERT INTO mascota (name, date_of_birth, weight_kg, neutered, escape_attempts, species_id, owner_id)
-values('Charmander', '02/08/2020', 11, false, 0, 1, 4);
-INSERT INTO mascota (name, date_of_birth, weight_kg, neutered, escape_attempts, species_id, owner_id)
-values('Plantmon', '15/11/2022', 5.7, true, 2,2,3);
-INSERT INTO mascota (name, date_of_birth, weight_kg, neutered, escape_attempts, species_id, owner_id)
-values('Squirtle', '02/04/1993', 12.13, false, 3,1,4);
-INSERT INTO mascota (name, date_of_birth, weight_kg, neutered, escape_attempts, species_id, owner_id)
-values('Angemon', '12/01/2005', 45, true, 1,2,5);
-INSERT INTO mascota (name, date_of_birth, weight_kg, neutered, escape_attempts, species_id, owner_id)
-values('Boarmon', '07/06/2005', 20.4, true, 7,2,5);
-INSERT INTO mascota (name, date_of_birth, weight_kg, neutered, escape_attempts, species_id, owner_id)
+VALUES ('Agumon', '02/03/2020', 10.23, true, 0, 2, 1),
+VALUES ('Gabumon', '11/15/2018', 5, true, 2, 2, 2),
+VALUES ('Pikachu', '01/07/2021', 15.04,false, 1, 1,2 ),
+VALUES ('Devimon', '05/12/2017', 11, true, 5, 2,3),
+values('Charmander', '02/08/2020', 11, false, 0, 1, 4),
+values('Plantmon', '15/11/2022', 5.7, true, 2,2,3),
+values('Squirtle', '02/04/1993', 12.13, false, 3,1,4),
+values('Angemon', '12/01/2005', 45, true, 1,2,5),
+values('Boarmon', '07/06/2005', 20.4, true, 7,2,5),
 values('Blossom', '13/10/1998', 17, true, 3,1,4);
+
+-- vets
+
+INSERT INTO vets (name, age, date_of_graduation)
+VALUES
+  ('William Tatcher', 45, '2000-4-23'),
+  ('Maisy Smith', 26, '2019-1-17'),
+  ('Stephanie Mendez', 64, '1981-5-4'),
+  ('Jack Harkness', 38, '2008-6-8');
+
+  INSERT INTO specializations (species_id, vet_id)
+VALUES
+  (1, 1),
+  (1, 3),
+  (2, 3),
+  (2, 4);
+
+--   visits
+
+INSERT INTO visits (animals_id, vets_id, date_of_visit)
+VALUES
+  (1, 1, '2020-5-24'),
+  (1, 3, '2020-7-22'),
+  (2, 4, '2021-2-2'),
+  (3, 2, '2020-1-5'),
+  (3, 2, '2020-3-8'),
+  (3, 2, '2020-5-14'),
+  (4, 3, '2021-5-4'),
+  (5, 4, '2021-2-24'),
+  (6, 2, '2019-12-21'),
+  (6, 1, '2020-8-10'),
+  (6, 2, '2021-4-7'),
+  (7, 3, '2019-9-29'),
+  (8, 4, '2020-10-3'),
+  (8, 4, '2020-11-4'),
+  (9, 2, '2019-1-24'),
+  (9, 2, '2019-5-15'),
+  (9, 2, '2020-2-27'),
+  (9, 2, '2020-8-3'),
+  (10, 3, '2020-5-24'),
+  (10, 1, '2021-1-11');
